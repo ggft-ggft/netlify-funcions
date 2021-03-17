1815baa7fb73f51f6090f8bb47158ccfa473dd73
Giuliano De Ioanni <giuliano.deioanni@gft.com>
2021-03-17 17:57:54 +0100

    sti cazzo di font

--- functions/hello.js
+++ functions/hello.js
@@ -9,9 +9,9 @@ dotenv.config();
 /* eslint-disable no-undef */
 console.log(
   "ENV",
-  process.env.CLOUDINARY_API_NAME, 
-  process.env.CLOUDINARY_API_KEY, 
-  process.env.CLOUDINARY_API_SECRET 
+  process.env.CLOUDINARY_API_NAME,
+  process.env.CLOUDINARY_API_KEY,
+  process.env.CLOUDINARY_API_SECRET
 );
 
 cloudinary.config({
@@ -21,7 +21,8 @@ cloudinary.config({
 });
 /* eslint-enable no-undef */
 
-exports.handler = async (event) => { // eslint-disable-line no-undef
+exports.handler = async (event) => {
+  // eslint-disable-line no-undef
   // Only allow POST
   if (event.httpMethod !== "POST") {
     return { statusCode: 405, body: "Method Not Allowed" };
@@ -36,34 +37,26 @@ exports.handler = async (event) => { // eslint-disable-line no-undef
     (payload?.id || generatoreService.makeid(10)) +
     generatoreService.getDateHash();
 
-
   console.log("DIRNAME:: ", __dirname); // eslint-disable-line no-undef
   console.log("TASK ROOT:: ", process.env.LAMBDA_TASK_ROOT); // eslint-disable-line no-undef
   //importazione font custom
-   fabric.nodeCanvas.registerFont(
-    process.env.LAMBDA_TASK_ROOT + "/fonts/LibreBaskerville-Regular.ttf",
-    {
-      family: "LibreBaskerville",
-      weight: "regular",
-      style: "normal",
-    }
-  );
-  fabric.nodeCanvas.registerFont(process.env.LAMBDA_TASK_ROOT + "/fonts/LibreBaskerville-Bold.ttf",
-    {
-      family: "LibreBaskerville",
-      weight: "bold",
-      style: "normal",
-    }
-  );
-  fabric.nodeCanvas.registerFont(process.env.LAMBDA_TASK_ROOT + "/fonts/LibreBaskerville-Italic.ttf",
-    {
-      family: "LibreBaskerville",
-      weight: "regular",
-      style: "italic",
-    }
-  );
+  fabric.nodeCanvas.registerFont("mbres/assets/fonts/LibreBaskerville-Regular.ttf", {
+    family: "LibreBaskerville",
+    weight: "regular",
+    style: "normal",
+  });
+  fabric.nodeCanvas.registerFont("mbres/assets/onts/LibreBaskerville-Bold.ttf", {
+    family: "LibreBaskerville",
+    weight: "bold",
+    style: "normal",
+  });
+  fabric.nodeCanvas.registerFont("mbres/assets/fonts/LibreBaskerville-Italic.ttf", {
+    family: "LibreBaskerville",
+    weight: "regular",
+    style: "italic",
+  });
   var canvas;
-  
+
   try {
     canvas = new fabric.StaticCanvas(null, {
       width: canvasDimensions.canvasWidth,
@@ -110,7 +103,7 @@ exports.handler = async (event) => { // eslint-disable-line no-undef
   } catch (error) {
     return { statusCode: 501, body: "Errore render" };
   }
-  
+
   const base64ImageOutput = canvas.toDataURL();
   console.log("STEP6");
   try {
@@ -137,7 +130,7 @@ exports.handler = async (event) => { // eslint-disable-line no-undef
   } else {
     return {
       statusCode: 200,
-      body: `Hello, ${name} \r\n ${filename} \r\n ${base64ImageOutput}`
+      body: `Hello, ${name} \r\n ${filename} \r\n ${base64ImageOutput}`,
     };
   }
 };
