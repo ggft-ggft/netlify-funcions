bbf556cbee6b2b6887be38fe105575b1d29f60e2
Giuliano De Ioanni <giuliano.deioanni@gft.com>
2021-03-18 12:43:10 +0100

    no fontz

--- functions/hello.js
+++ functions/hello.js
@@ -55,7 +55,7 @@ exports.handler = async (event) => {
   console.log("DIRNAME:: ", __dirname); // eslint-disable-line no-undef
   console.log("TASK ROOT:: ", process.env.LAMBDA_TASK_ROOT); // eslint-disable-line no-undef
   //importazione font custom
-  fabric.nodeCanvas.registerFont(path.resolve("./fonts/LibreBaskerville-Regular.ttf"), {
+  /* fabric.nodeCanvas.registerFont(path.resolve("./fonts/LibreBaskerville-Regular.ttf"), {
     family: "LibreBaskerville",
     weight: "regular",
     style: "normal",
@@ -69,7 +69,7 @@ exports.handler = async (event) => {
     family: "LibreBaskerville",
     weight: "regular",
     style: "italic",
-  });
+  }); */
   var canvas;
 
   try {
